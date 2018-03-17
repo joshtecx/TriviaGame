781a8bf7935f2181b5ebbd85cd8627c09918a80f
joshtecx <32961912+joshtecx@users.noreply.github.com>
2018-03-17 00:00:23 -0400

    updated with CSS and a start on a reset button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,17 @@
-// This code will run as soon as the page loads
-// window.onload = function() {
-    
-//     $("#answers1").on("click", selectAnswer);
+// I DECIDED TO ADD A RESET FUNCTION / BUTTON LATE FRIDAY NIGHT 
+// I WAS HAVING AN ISSUE WITH THE TIME RESETTING
+// I CAN FIGURE IT OUT WITH A LITTLE MORE WORK
+// NEEEEEED SLEEEP
+// ==============================================================================================================================
 
-// };
+
 
 $(document).ready(function(){
     $("#start").on("click", gameStart);
+    // $("#restart").on("click", gameReStart);
+
+
+
     // Global Variables
     // ===============================================================================
 
@@ -18,12 +23,21 @@ $(document).ready(function(){
         answer1: "Matt Groening ",
         question2: "Who is Homer's boss?",
         options2: ["Lenny Leonard ", "Carl Carlson ", "Waylon Smithers ", "Montgomery Burns "],
-        answer2: "Montgomery Burns "
+        answer2: "Montgomery Burns ",
+        question3: "What is the name of the Springfield Baseball team?",
+        options3: ["Atoms ", "Isotopes ", "Protons ", "Electrons "],
+        answer3: "Isotopes ",
+        question4: "What is the name of the annual Simpsons Halloween episode?",
+        options4: ["Shriekshow of Terror ", "Halloween Horror Night ", "Treehouse of Horror ", "Spooky Simpsons "],
+        answer4: "Treehouse of Horror ",
+        question5: "Which character was NOT voiced by Hank Azaria? ",
+        options5: ["Apu ", "Ned Flanders ", "Chief Wiggum ", "Comic Book Guy "],
+        answer5: "Ned Flanders "
     };
 
     var correct = 0;
     var incorrect = 0;
-    var countdown= 60;
+    var countdown= 30;
     var intervalId;
     var clockRunning = false;
     var selection;
@@ -38,17 +52,21 @@ $(document).ready(function(){
     function beginning(){
         $("#clock").hide();
         $("#questionDisplay").hide();
+        $("#resultsDisplay").hide();
+        $("#restart").hide();
     }
 
     // REMEMBER TO UNCOMMENT THIS WHEN YOU'RE DONE WITH THE REST OF THE GAME!!!
-    // beginning();
+    beginning();
 
 
     // Displpays questions and answers
     function displayQuestion(){
         $("#question1").append(questionAnswer.question1);
         $("#question2").append(questionAnswer.question2);
-
+        $("#question3").append(questionAnswer.question3);
+        $("#question4").append(questionAnswer.question4);
+        $("#question5").append(questionAnswer.question5);
     }
 
     displayQuestion();
@@ -58,7 +76,8 @@ $(document).ready(function(){
     function displayAnswers(){
         for (var i = 0; i < questionAnswer.options1.length; i++){
             var answerBtn = $("<button>");
-            answerBtn.addClass("options");
+            answerBtn.addClass("options1");
+            answerBtn.val("submit");
             answerBtn.attr("data-options",questionAnswer.options1[i]);
             answerBtn.text(questionAnswer.options1[i]);
             $("#answers1").append(answerBtn)
@@ -66,21 +85,50 @@ $(document).ready(function(){
 
         for (var j = 0; j < questionAnswer.options2.length; j++){
             var answerBtn = $("<button>");
-            answerBtn.addClass("options");
+            answerBtn.addClass("options2");
             answerBtn.attr("data-options",questionAnswer.options2[j]);
             answerBtn.text(questionAnswer.options2[j]);
             $("#answers2").append(answerBtn)
+        }
+
+        for (var k = 0; k < questionAnswer.options3.length; k++){
+            var answerBtn = $("<button>");
+            answerBtn.addClass("options3");
+            answerBtn.attr("data-options",questionAnswer.options3[k]);
+            answerBtn.text(questionAnswer.options3[k]);
+            $("#answers3").append(answerBtn)
+        }
+
+        for (var l = 0; l < questionAnswer.options4.length; l++){
+            var answerBtn = $("<button>");
+            answerBtn.addClass("options4");
+            answerBtn.attr("data-options",questionAnswer.options4[l]);
+            answerBtn.text(questionAnswer.options4[l]);
+            $("#answers4").append(answerBtn)
+        }
+
+        for (var m = 0; m < questionAnswer.options5.length; m++){
+            var answerBtn = $("<button>");
+            answerBtn.addClass("options5");
+            answerBtn.attr("data-options",questionAnswer.options5[m]);
+            answerBtn.text(questionAnswer.options5[m]);
+            $("#answers5").append(answerBtn)
         }
     }
 
     displayAnswers();
 
+    // I know, I know DRY....please just bear with me while I figure this out
+    // I just want to get it to work before I clean it up
+
     // gathers user selection and determines if that is correct or incorrect
-    $(".options").click(function() {
-        console.log(this);
+    $(".options1").click(function() {
+        // console.log(this);
         selection = $(this).attr("data-options");
         
-        if (selection === questionAnswer.answer1 || selection === questionAnswer.answer2){
+        // Player cannot choose more than one answer
+        $(".options1").prop("disabled", true);
+        if (selection === questionAnswer.answer1){
             correct++;
             console.log("WOOOOHOOO!");
         } else {
@@ -89,17 +137,89 @@ $(document).ready(function(){
         }
     });
 
+    $(".options2").click(function() {
+        // console.log(this);
+        selection = $(this).attr("data-options");
+        
+        // Player cannot choose more than one answer
+        $(".options2").prop("disabled", true);
+        if (selection === questionAnswer.answer2){
+            correct++;
+            console.log("WOOOOHOOO!");
+        } else {
+            incorrect++;
+            console.log("D'OH!");
+        }
+    });
+
+    $(".options3").click(function() {
+        // console.log(this);
+        selection = $(this).attr("data-options");
+        
+        // Player cannot choose more than one answer
+        $(".options3").prop("disabled", true);
+        if (selection === questionAnswer.answer3){
+            correct++;
+            console.log("WOOOOHOOO!");
+        } else {
+            incorrect++;
+            console.log("D'OH!");
+        }
+    });
+
+    $(".options4").click(function() {
+        // console.log(this);
+        selection = $(this).attr("data-options");
+        
+        // Player cannot choose more than one answer
+        $(".options4").prop("disabled", true);
+        if (selection === questionAnswer.answer4){
+            correct++;
+            console.log("WOOOOHOOO!");
+        } else {
+            incorrect++;
+            console.log("D'OH!");
+        }
+    });
+
+    $(".options5").click(function() {
+        // console.log(this);
+        selection = $(this).attr("data-options");
+        
+        // Player cannot choose more than one answer
+        $(".options5").prop("disabled", true);
+        if (selection === questionAnswer.answer5){
+            correct++;
+            console.log("WOOOOHOOO!");
+        } else {
+            incorrect++;
+            console.log("D'OH!");
+        }
+    });
+
+
    
-    // Player cannot choose more than one answer
 
     // Game starts when player clicks start
     function gameStart() {
         console.log('start');
         $("#start").hide();
+        $("#restart").hide();
+        $("#resultsDisplay").hide();
         $("#clock").show();
         $("#questionDisplay").show();
         intervalId = setInterval(timer, 1000);
     }
+
+    // function gameReStart() {
+    //     clearInterval(intervalId);
+    //     $("#start").hide();
+    //     $("#restart").hide();
+    //     $("#resultsDisplay").hide();
+    //     $("#clock").show();
+    //     $("#questionDisplay").show();
+    //     intervalId = setInterval(timer, 1000);
+    // }
 
     // Game ends when player runs out of time
     function gameOver(){
@@ -108,6 +228,8 @@ $(document).ready(function(){
         $("#questionDisplay").hide();
         clearInterval(intervalId);
         // Page reveals the number of questions answered correctly or incorrectly 
+        $("#resultsDisplay").show();
+        // $("#restart").show();
         $("#correct").text("You got " + correct + " correct!");
         $("#incorrect").text("You got " + incorrect + " incorrect!");
     }
